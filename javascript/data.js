a30af1cbc6fa244684a969583779222904663795
Gabriel Ferracioli <48016558+gabriel3224@users.noreply.github.com>
2021-02-28 23:52:27 -0300

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -1,3 +1,4 @@
+// Vetor com os elementos que serão repetidos exaustivamente
 export const data = [
     {
         id: 1,
@@ -8,10 +9,10 @@ export const data = [
     },
     {
         id: 2,
-        miniature_img: "https://th.bing.com/th/id/R8fa01c6357d96f9d5040667401599a52?rik=%2bM3FUBZGEaOwPg&riu=http%3a%2f%2f4.bp.blogspot.com%2f_I2UhOJOMYgo%2fSrtuauKYIsI%2fAAAAAAAABUs%2fCz3CnDI1324%2fs1600%2f95_angels_cry.jpg&ehk=kUo8tYVjrGVlQJld1XWrtq8j61KVULf5%2bVgrh%2fXD6eo%3d&risl=&pid=ImgRaw",
-        title: "Angels Cry",
-        datetime: "1993",
-        text: "Só Andre Matos é Andre Matos"
+        miniature_img: "https://fiorerouge.files.wordpress.com/2012/06/angra-holy-land-front-wallpaper.jpg",
+        title: "Holy Land",
+        datetime: "1996",
+        text: "Outro patamar né broder"
     }
 ];
   
