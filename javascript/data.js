89c1c247c8728896bbd0159eaaaab09681418aa3
Gabriel Ferracioli <48016558+gabriel3224@users.noreply.github.com>
2021-03-01 23:55:46 -0300

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -3,7 +3,7 @@ export const data = [
     {
         id: 1,
         link: "https://github.com/AndreSFND/Bomberman-Arduino",
-        miniature_img: "../assets/miniature_img/bomberman.png",
+        miniature_img: "assets/miniature_img/bomberman.png",
         title: "Bomberman em Arduino",
         datetime: "Julho de 2019",
         text: "Projeto final apresentado na matéria de Eletrônica para Computação, no qual fizemos um jogo similar ao Bomberman rodando em uma placa de Arduino. Mesmo que a minha participação na programação desse projeto seja nula, eu gostei bastante de participar dele pois contribuí desenvolvendo os sprites, e também a estrutura do console apresentado. A demonstração do funcionamento dele pode ser vista em https://youtu.be/RWOv7b1xpcc"
@@ -11,7 +11,7 @@ export const data = [
     {
         id: 2,
         link: "https://andresfnd.itch.io/catnap",
-        miniature_img: "../assets/miniature_img/catnap.jpg",
+        miniature_img: "assets/miniature_img/catnap.jpg",
         title: "Catnap",
         datetime: "Setembro de 2020",
         text: "Jogo criado em grupo com alguns amigos que foi usado para uma Game Jam que ocorreu no ano de 2020. Desenvolvemos em 3 dias um jogo no qual ajudamos o gato que ficou trancado para fora de casa a voltar para o conforno de seu lar. Minha contribuição no projeto foi na criando algumas das artes apresentadas, como o cenário e sprites dos gatos andando. A ferramenta que usei para isso foi a GIMP."
@@ -19,7 +19,7 @@ export const data = [
     {
         id: 3,
         link: "https://github.com/AndreSFND/desmipstificando",
-        miniature_img: "../assets/miniature_img/bomberman.png",
+        miniature_img: "assets/miniature_img/bomberman.png",
         title: "Desmipstificando",
         datetime: "Outubro de 2020",
         text: "Trabalho apresentado na disciplina de Organização e Arquitetura de Computadores, com o objetivo de criar alguma plataforma de ensino da arquitetura Assembly MIPS, assim, a plataforma mostrada é capaz de ensinar tópicos teóricos e práticos do tema."
@@ -27,7 +27,7 @@ export const data = [
     {
         id: 4,
         link: "https://github.com/AndreSFND/Overkibe",
-        miniature_img: "../assets/miniature_img/overkibe.png",
+        miniature_img: "assets/miniature_img/overkibe.png",
         title: "Overkibe",
         datetime: "Dezembro de 2020",
         text: "Jogo apresentado como trabalho final da matéria Sistemas Operacionais, com o objetivo de exemplificar o funcionamento dos processos do computador de maneira lúdica. Toda a arte do jogo foi criada por mim, e ela foi feita com a ferramenta GIMP."
@@ -35,7 +35,7 @@ export const data = [
     {
         id: 5,
         link: "https://icmcjunior.com.br/",
-        miniature_img: "../assets/miniature_img/icmcjr.jpg",
+        miniature_img: "assets/miniature_img/icmcjr.jpg",
         title: "ICMC Júnior",
         datetime: "Março de 2020 - Janeiro de 2021",
         text: "Empresa Júnior de computação localizada dentro do ICMC. Aprendi muito com ela a respeito do empreendedorismo, e também fiz muitas conexões neste período. Dentro dela atuei como assessor de projetos, e participei ativamente do projeto que será apresentado a seguir."
@@ -43,7 +43,7 @@ export const data = [
     {
         id: 6,
         link: "https://play.google.com/store/apps/details?id=com.ICMCJunior.EcoFriends",
-        miniature_img: "../assets/miniature_img/ecofriends.jpeg",
+        miniature_img: "assets/miniature_img/ecofriends.jpeg",
         title: "Ecofriends",
         datetime: "Disponibilizado na Play Store em março de 2021",
         text: "Projeto no qual participei quando estava na ICMC Júnior, o Ecofriends é um jogo com cunho ambiental, e visa conscientizar as crianças a respeito das mudanças climáticas no planeta. Neste jogo, atuei criando a arte de alguns dos jogos 2D e 3D, usando para isso as ferramentas GIMP e Blender entre 2020 e 2021."
@@ -51,10 +51,11 @@ export const data = [
     {
         id: 7,
         link: "https://gapsi.icmc.usp.br/",
-        miniature_img: "../assets/miniature_img/gapsi.png",
+        miniature_img: "assets/miniature_img/gapsi.png",
         title: "GAPsi",
         datetime: "Setembro de 2019 - Julho de 2021",
         text: "Sou bolsista PUB encarregado de cuidar da divulgação do GAPsi, o Grupo de Apoio Psicopedagógico que se originou no ICMC. Grupo que visa cuidar da saúde mental de todos que vivem no ambiente universitário. Neste projeto, estou atuo cuidando da página de divulgação do grupo, e também das redes sociais Facebook e Instagram para divulgação. Para isso, uso PHP, MySQL e  Javascript na programação, além de usar a ferramenta Canva para a criação dos posts."
     }
 ];
+  
   
