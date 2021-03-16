3c0e78b2d1f852aa25eee954e5a621ad470a13e5
Gabriel Ferracioli <ferracioligabriel@usp.br>
2021-03-16 18:48:36 -0300

    Finalização da versão inicial do site

--- javascript/data.js
+++ javascript/data.js
@@ -1,15 +1,7 @@
-// Vetor com os elementos que serão repetidos exaustivamente
+// Array with the elements that will be repeated in HTML
 export const data = [
     {
         id: 1,
-        link: "https://github.com/AndreSFND/Bomberman-Arduino",
-        miniature_img: "assets/miniature_img/bomberman.png",
-        title: "Bomberman em Arduino",
-        datetime: "Julho de 2019",
-        text: "Projeto final apresentado na matéria de Eletrônica para Computação, no qual fizemos um jogo similar ao Bomberman rodando em uma placa de Arduino. Mesmo que a minha participação na programação desse projeto seja nula, eu gostei bastante de participar dele pois contribuí desenvolvendo os sprites, e também a estrutura do console apresentado. A demonstração do funcionamento dele pode ser vista em https://youtu.be/RWOv7b1xpcc"
-    },
-    {
-        id: 2,
         link: "https://andresfnd.itch.io/catnap",
         miniature_img: "assets/miniature_img/catnap.jpg",
         title: "Catnap",
@@ -17,7 +9,7 @@ export const data = [
         text: "Jogo criado em grupo com alguns amigos que foi usado para uma Game Jam que ocorreu no ano de 2020. Desenvolvemos em 3 dias um jogo no qual ajudamos o gato que ficou trancado para fora de casa a voltar para o conforno de seu lar. Minha contribuição no projeto foi na criando algumas das artes apresentadas, como o cenário e sprites dos gatos andando. A ferramenta que usei para isso foi a GIMP."
     },
     {
-        id: 3,
+        id: 2,
         link: "https://github.com/AndreSFND/desmipstificando",
         miniature_img: "assets/miniature_img/desmipstificando.png",
         title: "Desmipstificando",
@@ -25,7 +17,7 @@ export const data = [
         text: "Trabalho apresentado na disciplina de Organização e Arquitetura de Computadores, com o objetivo de criar alguma plataforma de ensino da arquitetura Assembly MIPS, assim, a plataforma mostrada é capaz de ensinar tópicos teóricos e práticos do tema."
     },
     {
-        id: 4,
+        id: 3,
         link: "https://github.com/AndreSFND/Overkibe",
         miniature_img: "assets/miniature_img/overkibe.png",
         title: "Overkibe",
@@ -33,7 +25,7 @@ export const data = [
         text: "Jogo apresentado como trabalho final da matéria Sistemas Operacionais, com o objetivo de exemplificar o funcionamento dos processos do computador de maneira lúdica. Toda a arte do jogo foi criada por mim, e ela foi feita com a ferramenta GIMP."
     },
     {
-        id: 5,
+        id: 4,
         link: "https://icmcjunior.com.br/",
         miniature_img: "assets/miniature_img/icmcjr.jpg",
         title: "ICMC Júnior",
@@ -41,7 +33,7 @@ export const data = [
         text: "Empresa Júnior de computação localizada dentro do ICMC. Aprendi muito com ela a respeito do empreendedorismo, e também fiz muitas conexões neste período. Dentro dela atuei como assessor de projetos, e participei ativamente do projeto que será apresentado a seguir."
     },
     {
-        id: 6,
+        id: 5,
         link: "https://play.google.com/store/apps/details?id=com.ICMCJunior.EcoFriends",
         miniature_img: "assets/miniature_img/ecofriends.jpeg",
         title: "Ecofriends",
@@ -49,7 +41,7 @@ export const data = [
         text: "Projeto no qual participei quando estava na ICMC Júnior, o Ecofriends é um jogo com cunho ambiental, e visa conscientizar as crianças a respeito das mudanças climáticas no planeta. Neste jogo, atuei criando a arte de alguns dos jogos 2D e 3D, usando para isso as ferramentas GIMP e Blender entre 2020 e 2021."
     },
     {
-        id: 7,
+        id: 6,
         link: "https://gapsi.icmc.usp.br/",
         miniature_img: "assets/miniature_img/gapsi.png",
         title: "GAPsi",
@@ -57,5 +49,4 @@ export const data = [
         text: "Sou bolsista PUB encarregado de cuidar da divulgação do GAPsi, o Grupo de Apoio Psicopedagógico que se originou no ICMC. Grupo que visa cuidar da saúde mental de todos que vivem no ambiente universitário. Neste projeto, estou atuo cuidando da página de divulgação do grupo, e também das redes sociais Facebook e Instagram para divulgação. Para isso, uso PHP, MySQL e  Javascript na programação, além de usar a ferramenta Canva para a criação dos posts."
     }
 ];
-  
   
---
+++ javascript/utilities.js
@@ -0,0 +1,45 @@
+// Permite a troca entre Light mode e Dark mode
+function changeMode() {
+    
+    var navbar = document.getElementById("navbar");
+
+    var modeIcon = document.getElementById("change-mode").getAttribute("src");
+    console.log("Ícone atual: " + modeIcon);
+
+    if(navbar.classList.contains("dark-navbar")) {
+        // Ativa o Light mode
+
+        // Muda o cabeçalho
+        navbar.setAttribute("class", "light-navbar");
+        modeIcon = modeIcon.replace("Lght", "Dark");
+        console.log("Agora é " + modeIcon);
+        document.getElementById("change-mode").setAttribute("src", modeIcon);
+        document.getElementById("page-name").setAttribute("class", "black");
+        document.getElementById("button-1").setAttribute("class", "header-link light-header-link black");
+        document.getElementById("button-2").setAttribute("class", "header-link light-header-link black");
+        document.getElementById("button-3").setAttribute("class", "header-link light-header-link black");
+
+        // Muda o corpo do texto
+        document.getElementById("page-body").setAttribute("class", "vertical center white-background transition");
+        document.getElementById("intro").setAttribute("class", "black");
+        document.getElementById("easter-egg").setAttribute("class", "white transition");
+    }
+    else {
+        // Ativa o Dark mode
+
+        // Muda o cabeçalho
+        navbar.setAttribute("class", "dark-navbar");
+        modeIcon = modeIcon.replace("Dark", "Lght");
+        console.log("Agora é " + modeIcon);
+        document.getElementById("change-mode").setAttribute("src", modeIcon);
+        document.getElementById("page-name").setAttribute("class", "white");
+        document.getElementById("button-1").setAttribute("class", "header-link dark-header-link white");
+        document.getElementById("button-2").setAttribute("class", "header-link dark-header-link white");
+        document.getElementById("button-3").setAttribute("class", "header-link dark-header-link white");
+
+        // Muda o corpo do texto
+        document.getElementById("page-body").setAttribute("class", "vertical center black-background transition");
+        document.getElementById("intro").setAttribute("class", "white");
+        document.getElementById("easter-egg").setAttribute("class", "black transition");
+    }
+}
