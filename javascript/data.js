4079c2e31a284ba3ca374099f71aa4dbfa7c9cbe
Gabriel Ferracioli <ferracioligabriel@usp.br>
2022-02-07 12:33:24 -0300

    Inversão de vetor de dados

--- javascript/data.js
+++ javascript/data.js
@@ -2,38 +2,14 @@
 export const data = [
     {
         id: 1,
-        link: "https://andresfnd.itch.io/catnap",
-        miniature_img: "assets/miniature_img/catnap.jpg",
-        title: "Catnap",
-        datetime: "Setembro de 2020",
-        text: "Jogo criado em grupo com alguns amigos que foi usado para uma Game Jam que ocorreu no ano de 2020. Desenvolvemos em 3 dias um jogo no qual ajudamos o gato que ficou trancado para fora de casa a voltar para o conforno de seu lar. Minha contribuição no projeto foi a criação de algumas das artes apresentadas, como o cenário e sprites dos gatos andando. A ferramenta que usei para isso foi a GIMP."
+        link: "https://gapsi.icmc.usp.br/",
+        miniature_img: "assets/miniature_img/gapsi.png",
+        title: "GAPsi",
+        datetime: "Setembro de 2019 - Agosto de 2021",
+        text: "Sou bolsista PUB encarregado de cuidar da divulgação do GAPsi, o Grupo de Apoio Psicopedagógico que se originou no ICMC. Grupo que visa cuidar da saúde mental de todos que vivem no ambiente universitário. Neste projeto, atuo cuidando da página de divulgação do grupo, e também das redes sociais Facebook e Instagram para divulgação. Para isso, uso PHP, MySQL e  Javascript na programação, além de usar a ferramenta Canva para a criação dos posts."
     },
     {
         id: 2,
-        link: "https://github.com/AndreSFND/desmipstificando",
-        miniature_img: "assets/miniature_img/desmipstificando.png",
-        title: "Desmipstificando",
-        datetime: "Outubro de 2020",
-        text: "Trabalho apresentado na disciplina de Organização e Arquitetura de Computadores, com o objetivo de criar alguma plataforma de ensino da arquitetura Assembly MIPS, assim, a plataforma mostrada é capaz de ensinar tópicos teóricos e práticos do tema."
-    },
-    {
-        id: 3,
-        link: "https://github.com/AndreSFND/Overkibe",
-        miniature_img: "assets/miniature_img/overkibe.png",
-        title: "Overkibe",
-        datetime: "Dezembro de 2020",
-        text: "Jogo apresentado como trabalho final da matéria Sistemas Operacionais, com o objetivo de exemplificar o funcionamento dos processos do computador de maneira lúdica. Toda a arte do jogo foi criada por mim, e ela foi feita com a ferramenta GIMP."
-    },
-    {
-        id: 4,
-        link: "https://icmcjunior.com.br/",
-        miniature_img: "assets/miniature_img/icmcjr.jpg",
-        title: "ICMC Júnior",
-        datetime: "Março de 2020 - Janeiro de 2021",
-        text: "Empresa Júnior de computação localizada dentro do ICMC. Aprendi muito com ela a respeito do empreendedorismo, e também fiz muitas conexões neste período. Dentro dela atuei como assessor de projetos, e participei ativamente do projeto que será apresentado a seguir."
-    },
-    {
-        id: 5,
         link: "https://play.google.com/store/apps/details?id=com.ICMCJunior.EcoFriends",
         miniature_img: "assets/miniature_img/ecofriends.jpeg",
         title: "Ecofriends",
@@ -41,12 +17,35 @@ export const data = [
         text: "Projeto no qual participei quando estava na ICMC Júnior, o Ecofriends é um jogo com cunho ambiental, e visa conscientizar as crianças a respeito das mudanças climáticas no planeta. Neste jogo, atuei criando a arte de alguns dos jogos 2D e 3D, usando para isso as ferramentas GIMP e Blender entre 2020 e 2021."
     },
     {
+        id: 3,
+        link: "https://icmcjunior.com.br/",
+        miniature_img: "assets/miniature_img/icmcjr.jpg",
+        title: "ICMC Júnior",
+        datetime: "Março de 2020 - Janeiro de 2021",
+        text: "Empresa Júnior de computação localizada dentro do ICMC. Aprendi muito com ela a respeito do empreendedorismo, e também fiz muitas conexões neste período. Dentro dela atuei como assessor de projetos, e participei ativamente do projeto que será apresentado a seguir."
+    },
+    {
+        id: 4,
+        link: "https://github.com/AndreSFND/Overkibe",
+        miniature_img: "assets/miniature_img/overkibe.png",
+        title: "Overkibe",
+        datetime: "Dezembro de 2020",
+        text: "Jogo apresentado como trabalho final da matéria Sistemas Operacionais, com o objetivo de exemplificar o funcionamento dos processos do computador de maneira lúdica. Toda a arte do jogo foi criada por mim, e ela foi feita com a ferramenta GIMP."
+    },
+    {
+        id: 5,
+        link: "https://github.com/AndreSFND/desmipstificando",
+        miniature_img: "assets/miniature_img/desmipstificando.png",
+        title: "Desmipstificando",
+        datetime: "Outubro de 2020",
+        text: "Trabalho apresentado na disciplina de Organização e Arquitetura de Computadores, com o objetivo de criar alguma plataforma de ensino da arquitetura Assembly MIPS, assim, a plataforma mostrada é capaz de ensinar tópicos teóricos e práticos do tema."
+    },
+    {
         id: 6,
-        link: "https://gapsi.icmc.usp.br/",
-        miniature_img: "assets/miniature_img/gapsi.png",
-        title: "GAPsi",
-        datetime: "Setembro de 2019 - Agosto de 2021",
-        text: "Sou bolsista PUB encarregado de cuidar da divulgação do GAPsi, o Grupo de Apoio Psicopedagógico que se originou no ICMC. Grupo que visa cuidar da saúde mental de todos que vivem no ambiente universitário. Neste projeto, atuo cuidando da página de divulgação do grupo, e também das redes sociais Facebook e Instagram para divulgação. Para isso, uso PHP, MySQL e  Javascript na programação, além de usar a ferramenta Canva para a criação dos posts."
+        link: "https://andresfnd.itch.io/catnap",
+        miniature_img: "assets/miniature_img/catnap.jpg",
+        title: "Catnap",
+        datetime: "Setembro de 2020",
+        text: "Jogo criado em grupo com alguns amigos que foi usado para uma Game Jam que ocorreu no ano de 2020. Desenvolvemos em 3 dias um jogo no qual ajudamos o gato que ficou trancado para fora de casa a voltar para o conforno de seu lar. Minha contribuição no projeto foi a criação de algumas das artes apresentadas, como o cenário e sprites dos gatos andando. A ferramenta que usei para isso foi a GIMP."
     }
 ];
-  
